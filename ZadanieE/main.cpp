d57df42f6f94ba1db89dcc818701abbe558bf034
Denis <gwnkjell@mail.ru>
2020-09-27 23:24:31 +0500

    zadania

--- ZadanieE/main.cpp
+++ ZadanieE/main.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main()
 {
-int a,n,sum=1,x;
+int a,n,sum=1,x,m;
 cin>>a>>n;
 sum+=a;
 for (int i=1;i<n;i++){
-        x=a*a;
+        m=a*a;
+        x = m * a;
         sum+=x;
 }
 cout<<sum;
