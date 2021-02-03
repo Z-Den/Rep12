7143b183adea99e09870523075dfafb5b3940d6a
Z-Den <71875639+Z-Den@users.noreply.github.com>
2021-02-03 02:06:02 -0500

    кол-во программ с избегаемым этапом

---
+++ testing.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int f(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    if (n == 26) {
+        return 0;
+    }
+    if (n % 2 == 0) {
+        return f(n - 1);
+    }
+    if (n % 2 != 0 && n > 1) {
+        return f(n-1)+f((n - 1)/2);
+    }
+
+}
+int main() {
+    cout << f(27);
+}
