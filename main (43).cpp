fbbb3da36487eb82ae33b17bb16ee6664e7a0efd
Z-Den <71875639+Z-Den@users.noreply.github.com>
2021-02-13 11:40:51 +0500

    не больше N и делятся на каждую из своих цифр

---
+++ main (43).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        int x=i;
+        bool isCorrect=true;
+        while(x!=0){
+            int y=x%10;
+            if(x%10!=0 && i%y!=0){
+                isCorrect=false;
+            }
+            x/=10;
+        }
+        if(isCorrect){
+            cout<<i<<' ';
+        }
+    }
+
+    return 0;
+}
