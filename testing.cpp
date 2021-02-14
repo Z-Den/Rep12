f668eb887446a7848a3c60ac5ee27b030f47ef00
Z-Den <71875639+Z-Den@users.noreply.github.com>
2021-02-14 14:49:33 +0500

    Add files via upload

--- testing.cpp
+++ testing.cpp
@@ -1,20 +1,24 @@
 ﻿#include <iostream>
 using namespace std;
 int f(int n) {
-    if (n == 1) {
-        return 1;
-    }
-    if (n == 26) {
+    if (n == 8 || n == 2 || n == 1) {
         return 0;
     }
-    if (n % 2 == 0) {
+    if (n == 3) {
+        return 1;
+    }
+    if (n > 3) {
         return f(n - 1);
     }
-    if (n % 2 != 0 && n > 1) {
-        return f(n-1)+f((n - 1)/2);
+    if (n > 4) {
+        return f(n - 1) + f(n - 2);
     }
-
+    if (n > 5) {
+        return f(n - 1) + f(n - 2) + f(n - 3);
+    }
 }
 int main() {
-    cout << f(27);
+    for (int n = 1; n <= 15; n++) {
+        cout << f(n)<<endl;
+    }
 }
